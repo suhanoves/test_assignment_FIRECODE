28b342f410bcc3d8ec3a9c9bdc84c153cfd4f213
Eugene Suhanov <suhanoves@gmail.com>
2021-10-28 16:33:37 +0300

    add Street model

--- app/shops/admin.py
+++ app/shops/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 
-from shops.models import City
+from shops.models import City, Street
 
 
 @admin.register(City)
 class CityAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(Street)
+class StreetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city',)
+    list_select_related = ('city',)
+    list_filter = ('city',)
+    search_fields = ('name', 'city__name',)
--- app/shops/models.py
+++ app/shops/models.py
@@ -15,3 +15,31 @@ class City(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Street(models.Model):
+    name = models.CharField(
+        verbose_name='название улицы',
+        max_length=100,
+        db_index=True,
+    )
+    city = models.ForeignKey(
+        City,
+        on_delete=models.CASCADE,
+        verbose_name='город',
+        related_name='streets',
+        related_query_name='street',
+    )
+
+    class Meta:
+        verbose_name = 'улица'
+        verbose_name_plural = 'улицы'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('name', 'city'),
+                name='city_street_unique',
+            )
+        ]
+
+    def __str__(self):
+        return self.name
