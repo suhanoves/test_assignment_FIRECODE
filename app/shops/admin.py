e41fef7b3d35089114aabaf6cbb3f2b0e2e8b933
Eugene Suhanov <suhanoves@gmail.com>
2021-10-28 15:59:09 +0300

    add City model

--- app/shops/admin.py
+++ app/shops/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from shops.models import City
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    pass
--- app/shops/models.py
+++ app/shops/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(
+        verbose_name='название города',
+        max_length=50,
+        unique=True,
+        db_index=True,
+    )
+
+    class Meta:
+        verbose_name = 'город'
+        verbose_name_plural = 'города'
+
+    def __str__(self):
+        return self.name
