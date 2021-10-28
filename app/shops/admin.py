0e1bb7ac295ec052271ccaf5770859afd4e2032c
Eugene Suhanov <suhanoves@gmail.com>
2021-10-28 17:41:29 +0300

    add Shop model

--- app/shops/admin.py
+++ app/shops/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from shops.models import City, Street
+from shops.models import City, Street, Shop
 
 
 @admin.register(City)
@@ -14,3 +14,13 @@ class StreetAdmin(admin.ModelAdmin):
     list_select_related = ('city',)
     list_filter = ('city',)
     search_fields = ('name', 'city__name',)
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'city', 'street', 'building', 'opening_time', 'closing_time',
+    )
+    list_select_related = ('city', 'street',)
+    list_filter = ('city',)
+    search_fields = ('name', 'city__name', 'street__name',)
--- app/shops/models.py
+++ app/shops/models.py
@@ -43,3 +43,52 @@ class Street(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Shop(models.Model):
+    name = models.CharField(
+        verbose_name='название магазина',
+        max_length=150,
+        db_index=True,
+    )
+    city = models.ForeignKey(
+        City,
+        on_delete=models.CASCADE,
+        verbose_name='город',
+        related_name='shops',
+        related_query_name='shop',
+    )
+    street = models.ForeignKey(
+        Street,
+        on_delete=models.CASCADE,
+        verbose_name='улица',
+        related_name='shops',
+        related_query_name='shop',
+    )
+    building = models.CharField(
+        verbose_name='номер дома',
+        max_length=10,
+    )
+    opening_time = models.TimeField(
+        verbose_name='время открытия'
+    )
+    closing_time = models.TimeField(
+        verbose_name='время закрытия'
+    )
+
+    class Meta:
+        verbose_name = 'магазин'
+        verbose_name_plural = 'магазины'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('name', 'city', 'street', 'building'),
+                name='shop_city_street_building_unique',
+            ),
+            models.CheckConstraint(
+                check=models.Q(closing_time__gt=models.F('opening_time')),
+                name='closing_time_gt_opening_time'
+            )
+        ]
+
+    def __str__(self):
+        return self.name
