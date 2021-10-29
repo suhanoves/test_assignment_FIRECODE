ac731372fb7c7e63678f85212637dbd3e329a819
Eugene Suhanov <suhanoves@gmail.com>
2021-10-29 20:03:25 +0300

    add ViewSet endpoints for City, Street and Shop models

--- app/shops/views.py
+++ app/shops/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from shops.models import City, Street, Shop
+from shops.serializers import CitySerializer, StreetSerializer, ShopSerializer
+
+
+class CityViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    API endpoint that allows cities to be viewed or edited.
+    """
+    queryset = City.objects.all().order_by('name')
+    serializer_class = CitySerializer
+
+
+class StreetViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    API endpoint that allows streets to be viewed or edited.
+    """
+    queryset = Street.objects.all()
+    serializer_class = StreetSerializer
+
+
+class ShopViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows shops to be viewed or edited.
+    """
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
