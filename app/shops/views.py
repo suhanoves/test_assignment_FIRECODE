b538dbc7922e8eb076b4faa61eb008691b530909
Eugene Suhanov <suhanoves@gmail.com>
2021-10-31 13:20:51 +0300

    minor change docstrings

--- app/shops/views.py
+++ app/shops/views.py
@@ -6,17 +6,13 @@ from shops.serializers import CitySerializer, StreetSerializer, \
 
 
 class CityViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    API endpoint that allows cities to be viewed or edited.
-    """
+    """API endpoint that allows cities to be viewed or edited."""
     queryset = City.objects.all().order_by('name')
     serializer_class = CitySerializer
 
 
 class StreetViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    API endpoint that allows streets to be viewed or edited.
-    """
+    """API endpoint that allows streets to be viewed or edited."""
     queryset = Street.objects.all()
     serializer_class = StreetSerializer
 
