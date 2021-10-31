c72c24ddb69b86a7cdd4164020c89d1bea853a08
Eugene Suhanov <suhanoves@gmail.com>
2021-10-31 13:34:23 +0300

    Add and modify docstrings according to PEP 257

--- app/shops/serializers.py
+++ app/shops/serializers.py
@@ -1,17 +1,20 @@
 from rest_framework import serializers
-from rest_framework.serializers import ValidationError
 from rest_framework.validators import UniqueTogetherValidator
 
 from shops.models import City, Street, Shop
 
 
 class CitySerializer(serializers.ModelSerializer):
+    """Serializer for City model"""
+
     class Meta:
         model = City
         fields = ('id', 'name',)
 
 
 class StreetSerializer(serializers.ModelSerializer):
+    """Serializer for Street model"""
+
     class Meta:
         model = Street
         fields = ('id', 'name', 'city',)
@@ -24,12 +27,22 @@ class StreetSerializer(serializers.ModelSerializer):
 
 
 class ShopIdSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Shop model that only represents the Shop as
+    an id field after creating a new store
+    """
+
     class Meta:
         model = Shop
         fields = ('id',)
 
 
 class ShopDetailSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Shop model, that presents the relational fields
+    "city" and "street" as text when displaying a list of shops
+    """
+
     city = serializers.SlugRelatedField(slug_field='name', read_only=True)
     street = serializers.SlugRelatedField(slug_field='name', read_only=True)
 
@@ -42,6 +55,11 @@ class ShopDetailSerializer(serializers.ModelSerializer):
 
 
 class ShopCreateSerializer(serializers.ModelSerializer):
+    """
+    A serializer for the Shop model that represents all fields of the model
+    and validates the received data
+    """
+
     class Meta:
         model = Shop
         fields = (
@@ -76,5 +94,5 @@ class ShopCreateSerializer(serializers.ModelSerializer):
             )
 
         if errors:
-            raise ValidationError(errors)
+            raise serializers.ValidationError(errors)
         return data
--- app/shops/views.py
+++ app/shops/views.py
@@ -7,18 +7,21 @@ from shops.serializers import CitySerializer, StreetSerializer, \
 
 class CityViewSet(viewsets.ReadOnlyModelViewSet):
     """API endpoint that allows cities to be viewed or edited."""
+
     queryset = City.objects.all().order_by('name')
     serializer_class = CitySerializer
 
 
 class StreetViewSet(viewsets.ReadOnlyModelViewSet):
     """API endpoint that allows streets to be viewed or edited."""
+
     queryset = Street.objects.all()
     serializer_class = StreetSerializer
 
 
 class StreetOfCityView(generics.ListAPIView):
-    """API endpoint that allows streets of city to be viewed"""
+    """API endpoint that allows streets of city to be viewed."""
+
     serializer_class = StreetSerializer
 
     def get_queryset(self):
@@ -37,9 +40,8 @@ class StreetOfCityView(generics.ListAPIView):
 
 
 class ShopViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows shops to be viewed or edited.
-    """
+    """API endpoint that allows shops to be viewed or edited."""
+
     queryset = Shop.objects.all()
     serializer_class = ShopCreateSerializer
 
