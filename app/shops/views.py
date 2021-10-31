0835057304694bcaad367d2d3c950802c5e081aa
Eugene Suhanov <suhanoves@gmail.com>
2021-10-31 13:21:36 +0300

    add StreetOfCityView endpoint that allows streets of city to be viewed

--- app/shops/views.py
+++ app/shops/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import generics, viewsets, serializers
 
 from shops.models import City, Street, Shop
 from shops.serializers import CitySerializer, StreetSerializer, \
@@ -15,6 +15,25 @@ class StreetViewSet(viewsets.ReadOnlyModelViewSet):
     """API endpoint that allows streets to be viewed or edited."""
     queryset = Street.objects.all()
     serializer_class = StreetSerializer
+
+
+class StreetOfCityView(generics.ListAPIView):
+    """API endpoint that allows streets of city to be viewed"""
+    serializer_class = StreetSerializer
+
+    def get_queryset(self):
+        """
+        Return a list of all the streets for the city
+        as determined by the city_id of the URL.
+        """
+        city_id = self.kwargs['city_id']
+
+        if not City.objects.filter(id=city_id).exists():
+            raise serializers.ValidationError(
+                {"error": "города с таким id не существует"}
+            )
+
+        return Street.objects.filter(city_id=city_id).order_by('name')
 
 
 class ShopViewSet(viewsets.ModelViewSet):
