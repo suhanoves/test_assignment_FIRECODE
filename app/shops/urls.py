192ec14660855c5e968c0d04f7d78c0796b27f8a
Eugene Suhanov <suhanoves@gmail.com>
2021-10-31 13:18:12 +0300

    add path for 'city/<city_id>/street' url

--- app/shops/urls.py
+++ app/shops/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from shops.views import CityViewSet, StreetViewSet, ShopViewSet
+from shops.views import CityViewSet, StreetViewSet, ShopViewSet, StreetOfCityView
 
 router = routers.DefaultRouter()
 router.register(r'city', CityViewSet)
@@ -9,5 +9,6 @@ router.register(r'street', StreetViewSet)
 router.register(r'shop', ShopViewSet)
 
 urlpatterns = [
+    path('city/<int:city_id>/street/', StreetOfCityView.as_view()),
     path('', include(router.urls)),
 ]
